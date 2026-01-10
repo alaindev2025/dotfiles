cfe0cf1a483a02638e9450c7e570d8429177e605
alaindev <alain.segredo2004@gmail.com>
2026-01-10 14:21:47 -0500

    Added vim motions

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,10 @@ set -gx EDITOR nvim
 set -gx VISUAl nvim
 set -gx PAGER less
 set -U fish_greeting
+set -g fish_vi_key_bindings
 
-if type -q zoxide 
-  zoxide init fish | source
+if type -q zoxide
+    zoxide init fish | source
 end
 
 # if type -q oh-my-posh
@@ -12,9 +13,9 @@ end
 # end
 
 if type -q starship
-  starship init fish | source
+    starship init fish | source
 end
 
 if type -q atuin
-  atuin init fish | source
+    atuin init fish | source
 end
